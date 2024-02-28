afd736d2cc66465ac59c1d5451af7179d4b7a4f0
danaorel <danyca.oryol@gmail.com>
2024-02-28 14:13:40 +0300

    2 задание

---
+++ 2 задание.py
@@ -0,0 +1,12 @@
+a=input("enter seat number:")
+a=int(a)
+if a%2==0 and a<37:
+    print('upper coupe bed')
+if a%2!=0 and a<37:
+    print('bottom coupe bed')
+if a%2==0 and a>37:
+    print('upper side bed')
+if a%2!=0 and a>=37:
+    print("bottom side bed")
+if a>52:
+    print("you have bed in the next cab")
--- main.py
+++ main.py
@@ -1,13 +1,6 @@
-les 2
-a=input("enter seat number:")
-a=int(a)
-if a%2==0 and a<37:
-    print('upper coupe bed')
-if a%2!=0 and a<37:
-    print('bottom coupe bed')
-if a%2==0 and a>37:
-    print('upper side bed')
-if a%2!=0 and a>=37:
-    print("bottom side bed")
-if a>52:
-    print("you have bed in the next cab")
+a=input('enter password:')
+b=input('confirm password:')
+if a==b:
+    print('password is correct')
+else:
+    print('incorrect password')

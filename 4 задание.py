dbaffb71ad36f95a7ade8609bb9ab17c229180b6
danaorel <danyca.oryol@gmail.com>
2024-02-28 14:15:38 +0300

    4 задание

---
+++ 4 задание.py
@@ -0,0 +1,9 @@
+a= input('set collor:')
+if a=='red blue':
+    print('purpule')
+if a=='red yellow':
+    print('orange')
+if a=='blue yellow':
+    print('green')
+else:
+    print('error occured')

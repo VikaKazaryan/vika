bc291bb9c495da923c78500ec519e8f5da42757a
Sonechka19 <sofaposss29@gmail.com>
2024-03-20 13:54:13 +0300

    2

---
+++ 4 лаба/2.py
@@ -0,0 +1,12 @@
+def task2():
+    def delit(x):
+        itog = 300 / x
+        return itog
+    try:
+        x = int(input("введите число: "))
+        print(delit(x))
+    except ValueError:
+        print("Ошибка! Введено не число!")
+    except ZeroDivisionError:
+        print("Ошибка! Деление на 0 невозможно!")
+task2()
